ffd9692302a06dbe32fc6451a8a507310d4dac2e
zhang <zz593141477@gmail.com>
2015-02-21 17:20:35 +0800

    add more crypto modules

---
+++ Project/src/crypto/base64.h
@@ -0,0 +1,23 @@
+/*******************************************************************
+
+Base64 Encoding/Decoding Library
+
+Author: Marcin Kelar ( marcin.kelar@holicon.pl )
+*******************************************************************/
+#ifndef BASE64_H
+#define BASE64_H
+
+#include <stdio.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+size_t	base64_decode( char *source, unsigned char *target, size_t targetlen );
+int		base64_encode( unsigned char *source, size_t sourcelen, char *target, size_t targetlen );
+
+#ifdef __cplusplus
+};
+#endif
+#endif
